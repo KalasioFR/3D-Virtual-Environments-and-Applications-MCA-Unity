12171a1e73cdd4329d63b8c862760fc7609be06d
KalasioFR <kalasio.games1@gmail.com>
2024-03-24 20:30:41 +0200

    Added directions, sprint, and jump

---
+++ MCA-Unity/Assets/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInputs playerInput;
+    private PlayerInputs.OnFootActions onFoot;
+    private PlayerMotor motor;
+    private PlayerLook look;
+
+    void Awake()
+    {
+        playerInput = new PlayerInputs();
+        onFoot = playerInput.OnFoot;
+
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+
+        onFoot.Sprint.started += ctx => motor.Sprint();
+        onFoot.Sprint.canceled += ctx => motor.Sprint();
+        onFoot.Jump.performed += ctx => motor.Jump();
+    }
+
+    void FixedUpdate()
+    {
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
---
+++ MCA-Unity/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool isGrounded;
+    private bool sprinting = false;
+    public float speed = 5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+    }
+
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+        playerVelocity.y += gravity * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    public void Sprint()
+    {
+    
+        sprinting = !sprinting;
+        if (sprinting)
+        {
+            speed = 8;
+        }
+        else
+        {
+            speed = 5;
+        }
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3f * gravity);
+        }
+    }
+}
